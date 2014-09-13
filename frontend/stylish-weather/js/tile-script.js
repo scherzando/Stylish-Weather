a9d6d0ea7b4703c6f317e58e713e34954da844a6
scherzando <wipethefridge@gmail.com>
2014-09-14 11:39:24 +1200

    Script update
    Categories starting to display

--- frontend/stylish-weather/js/tile-script.js
+++ frontend/stylish-weather/js/tile-script.js
@@ -1,13 +1,37 @@
 
 function createTiles(jsonObj){
-	console.log('createTiles function');
-
 	var items = jsonObj.items;
-	
+
+	var tileBox = document.getElementById('tile-box');
+	var cont = document.createElement('div');
+
+	cont.className = "row";
+
 	for(var i = 0; i < items.length; i++){
-		console.log(i);
+		var title = document.createElement('h2');
+		title.className = "text-center";
+		title.innerHTML = items[i].name;
+		cont.appendChild(title);
+		cont.appendChild(document.createElement('hr'));
+		var cat = createCategory(items[i]);
+		cont.appendChild(cat);
 	}
 
+	tileBox.appendChild(cont);
+}
+
+// returns one category object to be appended to the DOM
+function createCategory(itemObj){
+	var categoryDiv = document.createElement('div');
+	categoryDiv.className = itemObj.name;
+
+	
+	var catUrl = itemObj.url;
+	var first = itemObj.first;
+	var second = itemObj.second;
+	console.log(second.title);
+
+	return categoryDiv;
 }
 
 var jObj = $.getJSON("test.json", function(json){
