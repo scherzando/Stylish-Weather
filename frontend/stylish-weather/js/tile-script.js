6dfea5ba325db3fddeff1837c0c2c35cacbb2ed8
scherzando <wipethefridge@gmail.com>
2014-09-14 10:59:20 +1200

    Json example structure
    in frontend folder

--- frontend/stylish-weather/js/tile-script.js
+++ frontend/stylish-weather/js/tile-script.js
@@ -1,3 +1,15 @@
 
-// loop json object
-// create tile for each item
+function createTiles(jsonObj){
+	console.log('createTiles function');
+
+	var items = jsonObj.items;
+	
+	for(var i = 0; i < items.length; i++){
+		console.log(i);
+	}
+
+}
+
+var jObj = $.getJSON("test.json", function(json){
+	createTiles(json);
+});
