3feb3d66bbc4739e6c4fb7ec9136be5eeee8daff
scherzando <wipethefridge@gmail.com>
2014-09-14 16:38:41 +1200

    Improve date format

--- Backend-2/UnknownBackend/UnknownBackend/stylish-weather/js/tile-script.js
+++ Backend-2/UnknownBackend/UnknownBackend/stylish-weather/js/tile-script.js
@@ -75,10 +75,31 @@ function createInnerCategory(catObj){
 	return col;
 }
 
+Date.prototype.getDayName = function() {
+	var d = ['Sunday','Monday','Tuesday','Wednesday',
+	'Thursday','Friday','Saturday'];
+	return d[this.getDay()];
+}
+
 // fill in header/weather information
 function fillHeader(weather){
+	var cityName = document.getElementById('city-name')
+	cityName.innerHTML = "WELLINGTON";
+
 	var dateText = document.getElementById('date-text');
-	dateText.innerHTML = weather.date;
+	// format date
+	var monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"];
+
+	var date = new Date(weather.date);
+	var day = date.getDate();
+	var month = monthNames[date.getMonth()];
+	var year = date.getFullYear();
+	var dayName = date.getDayName();
+
+	var dateString = dayName + " " + day + " " + month;
+
+	dateText.innerHTML = dateString;
 
 	var tempText = document.getElementById('temp-text');
 	tempText.innerHTML = weather.temp + "°C";
@@ -86,6 +107,7 @@ function fillHeader(weather){
 	var img = document.getElementById('weather-img');
 	img.setAttribute('src', weather.img);
 }
+
 
 $(document).ready(function() {
 
